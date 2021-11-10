f1d16a2e0b1893ba58f0dabba652aa6d7924a496
Malik <malikoabdulaziz@gmail.com>
2021-11-10 01:06:49 +0100

    implement requested changes

--- tensei-react-js-sdk-rest-todos-app/server/server.ts
+++ tensei-react-js-sdk-rest-todos-app/server/server.ts
@@ -1,11 +1,11 @@
-import { welcome, tensei, cors , resource, text} from '@tensei/core'
+import { welcome, tensei, cors , resource, text, boolean} from '@tensei/core'
 import { rest } from '@tensei/rest'
 
 
 
 export default tensei()
   .root(__dirname)
-  .resources([resource('Todolist').fields([text('todo'), text('complete_status').default("false")])])
+  .resources([resource('Todo').fields([text('Title'), boolean('Complete Status').default(false)])])
   .plugins([welcome(), rest().plugin(), cors()])
   .start()
   .catch(console.error)
